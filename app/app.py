00c03b6191b277aa0554a7fbdc7c4d251c46edfe
Elisa Sebastian <Elisa.sbstn@gmail.com>
2021-04-01 10:05:23 +0200

    Add probabilities

--- app/app.py
+++ app/app.py
@@ -1,48 +1,79 @@
+# Importation
 import base64
 import io
+import pickle
+import yaml
+
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
-
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
 import tensorflow as tf
+
+from dash.dependencies import Input, Output
+from constants import CLASSES
 from PIL import Image
 
-from constants import CLASSES
 
+#Téléchargement des paramètres de app.yaml
+with open('app.yaml') as yaml_app:
+    parameters = yaml.safe_load(yaml_app)
+    
+IMAGE_WIDTH = parameters['parameters']['IMAGE_WIDTH']
+IMAGE_HEIGHT = parameters['parameters']['IMAGE_HEIGHT']
+load_model_path_DNN = parameters['paths']['load_model_path_DNN']
+load_model_path_SVM = parameters['paths']['load_model_path_SVM']
 
-IMAGE_WIDTH = 30
-IMAGE_HEIGHT = IMAGE_WIDTH
+#Téléchargement des modèles
+DNN = tf.keras.models.load_model(load_model_path_DNN)
+SVM = pickle.load(open(load_model_path_SVM,'rb'))
 
-# Load DNN model
-classifier = tf.keras.models.load_model('../models/traffic_signs_2021-03-20_09-32-50.h5')
-
+#Prédiction de la classe de l'image et affichage des probabilités 
 def classify_image(image, model, image_box=None):
-  """Classify image by model
-
-  Parameters
-  ----------
-  content: image content
-  model: tf/keras classifier
-
-  Returns
-  -------
-  class id returned by model classifier
-  """
-  images_list = []
-  image = image.resize((IMAGE_WIDTH, IMAGE_HEIGHT), box=image_box)
+    """Classify image by model and show probabilities for each traffic signs
+    
+    Parameters
+    ---------
+    content: image content
+    model: tf/keras classifier
+    
+    Returns
+    -------
+    class id returned by model classifier
+    list of probabilities for each traffic signs
+    """
+    
+    images_list = []
+    image = image.resize((IMAGE_WIDTH, IMAGE_HEIGHT), box=image_box)
                                         # box argument clips image to (x1, y1, x2, y2)
-  image = np.array(image)
-  images_list.append(image)
-  
-  return model.predict_classes(np.array(images_list))
+    image = np.array(image)
+    
+    if model=='SVM':
+        image_color = image.flatten()
+        # convert images to greyscale
+        image =rgb2grey(image)
+        # combine color and hog features into a single array
+        flat_features = np.hstack(image_color)
+        images_list.append(flat_features)
+        
+        return model.predict(np.array(images_list)), model.predict_proba(np.array(images_list)).tolist()[0]
+    
+    else:
+        images_list.append(image)
+        return model.predict_classes(np.array(images_list)), model.predict(np.array(images_list)).tolist()[0]
 
+# Initialisation de l'app
 
-app = dash.Dash('Traffic Signs Recognition') #, external_stylesheets=dbc.themes.BOOTSTRAP)
+app = dash.Dash('Traffic Signs Recognition') 
 
+colors = {
+    'background': '#dfeef9',
+    'text': '#203675'
+}
 
 pre_style = {
     'whiteSpace': 'pre-wrap',
@@ -51,8 +82,19 @@ pre_style = {
 }
 
 
-# Define application layout
-app.layout = html.Div([
+
+# Layout de l'app
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(children='Prédiction des types de panneaux',
+            style={
+            'textAlign': 'center',
+            'color': colors['text'],
+            'verticalAlign': 'middle',
+        }), 
+     html.Div(children='Prédiction des types de panneaux et affichage des probabilités', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
     dcc.Upload(
         id='bouton-chargement',
         children=html.Div([
@@ -60,36 +102,39 @@ app.layout = html.Div([
                     html.A('sélectionner une image')
         ]),
         style={
-            'width': '50%',
+            'width': '25%',
             'height': '60px',
             'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
+            'borderWidth': '5px',
+            'borderStyle': 'double',
             'borderRadius': '5px',
             'textAlign': 'center',
-            'margin': '10px'
+            'margin': '25px',
+            'color': colors['text'],
+            'marginLeft': 'auto',
+            'marginRight': 'auto'
         }
     ),
-    html.Div(id='mon-image'),
-    dcc.Input(id='mon-champ-texte', value='valeur initiale', type='text'),
-    html.Div(id='ma-zone-resultat')
+    html.Div(id='mon-image')
 ])
 
+#Callback 
 @app.callback(Output('mon-image', 'children'),
               [Input('bouton-chargement', 'contents')])
+
 def update_output(contents):
     if contents is not None:
         content_type, content_string = contents.split(',')
         if 'image' in content_type:
             image = Image.open(io.BytesIO(base64.b64decode(content_string)))
-            predicted_class = classify_image(image, classifier)[0]
+            predicted_class = classify_image(image, DNN)[0][0]
+            proba_panneaux = classify_image(image, DNN)[1]
+            nb_class=np.arange(1,44)
             return html.Div([
-                html.Hr(),
-                html.Img(src=contents),
-                html.H3('Classe prédite : {}'.format(CLASSES[predicted_class])),
-                html.Hr(),
-                #html.Div('Raw Content'),
-                #html.Pre(contents, style=pre_style)
+                html.Img(src=contents,style={'height':'15%','width':'15%','marginLeft': '550px', 'marginRight': 'auto','borderRadius': '5px'}),
+                html.H3('Il y a {} % de chances que le panneau soit de type {}'.format(round((proba_panneaux[predicted_class])*100,2),CLASSES[predicted_class])),
+                dcc.Graph(id='coucou', figure=px.bar(x=nb_class, y=proba_panneaux, barmode="group", title="Probabilités pour chaque panneaux",
+                          labels={'x':'Type de panneaux', 'y':'Probabilité'})),
             ])
         else:
             try:
@@ -104,32 +149,23 @@ def update_output(contents):
                 img_bytes = buffer.read()
                 content_string = base64.b64encode(img_bytes).decode('ascii')
                 # Appel du modèle de classification
-                predicted_class = classify_image(image, classifier)[0]
+                predicted_class = classify_image(image, DNN)[0][0]
+                proba_panneaux = classify_image(image, DNN)[1]
+                nb_class=np.arange(1,44)
                 # Affichage de l'image
                 return html.Div([
-                    html.Hr(),
-                    html.Img(src='data:image/png;base64,' + content_string),
-                    html.H3('Classe prédite : {}'.format(CLASSES[predicted_class])),
-                    html.Hr(),
+                    html.Img(src='data:image/png;base64,' + content_string,style={'height':'15%','width':'15%','marginLeft': '550px', 'marginRight': 'auto','borderRadius': '5px'}),
+                    html.H3('Il y a {} % de chances que le panneau soit de type {}'.format(round((proba_panneaux[predicted_class])*100,2),CLASSES[predicted_class])),
+                    dcc.Graph(id='coucou', figure=px.bar(x=nb_class, y=proba_panneaux, barmode="group",title="Probabilités pour chaque panneaux",
+                              labels={'x':'Type de panneaux', 'y':'Probabilité'})),
                 ])
             except:
                 return html.Div([
-                    html.Hr(),
-                    html.Div('Uniquement des images svp : {}'.format(content_type)),
-                    html.Hr(),                
+                    html.Div('Uniquement des images svp : {}'.format(content_type)),             
                     html.Div('Raw Content'),
                     html.Pre(contents, style=pre_style)
                 ])
             
-
-# Manage interactions with callbacks
-@app.callback(
-    Output(component_id='ma-zone-resultat', component_property='children'),
-    [Input(component_id='mon-champ-texte', component_property='value')]
-)
-def update_output_div(input_value):
-    return html.H3('Valeur saisie ici "{}"'.format(input_value))
-
 
 # Start the application
 if __name__ == '__main__':
